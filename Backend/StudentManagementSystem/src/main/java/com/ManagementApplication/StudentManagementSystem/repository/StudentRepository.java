3c9908f2bbd00c01da84a649269ea3d253c33e52
Wickram2001 <wickramdarshana2001@gmail.com>
2025-05-14 23:59:46 +0530

    Update StudentRepository.java

--- Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/repository/StudentRepository.java
+++ Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/repository/StudentRepository.java
@@ -1,4 +1,25 @@
 package com.ManagementApplication.StudentManagementSystem.repository;
 
-public interface StudentRepository {
+import com.ManagementApplication.StudentManagementSystem.entity.Student;
+import com.ManagementApplication.StudentManagementSystem.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+
+    Optional<Student> findByStudentId(String studentId);
+
+    Optional<Student> findByUser(User user);
+
+    List<Student> findByDepartment(String department);
+
+    List<Student> findByEnrollmentYear(Integer enrollmentYear);
+
+    List<Student> findByCurrentSemester(String currentSemester);
+
+    boolean existsByStudentId(String studentId);
 }
