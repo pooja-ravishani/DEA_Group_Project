e691f0aaaf1fe7c4393156a227a896d7e71f1f24
Tharinda Rupasinghe <pasirupasinghe@gmail.com>
2025-05-15 00:19:37 +0530

    Update CourseService.java

--- Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/service/CourseService.java
+++ Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/service/CourseService.java
@@ -1,4 +1,34 @@
 package com.ManagementApplication.StudentManagementSystem.service;
 
+import com.studentmanagement.StudentManagementSystem.dto.CourseDTO;
+
+import java.util.List;
+
 public interface CourseService {
+
+    CourseDTO createCourse(CourseDTO courseDTO);
+
+    CourseDTO getCourseById(Integer id);
+
+    CourseDTO getCourseByCourseCode(String courseCode);
+
+    List<CourseDTO> getAllCourses();
+
+    List<CourseDTO> getCoursesByDepartment(String department);
+
+    List<CourseDTO> getCoursesByTeacherId(Integer teacherId);
+
+    List<CourseDTO> getActiveCourses();
+
+    CourseDTO updateCourse(Integer id, CourseDTO courseDTO);
+
+    void deleteCourse(Integer id);
+
+    boolean isCourseCodeExists(String courseCode);
+
+    void assignTeacherToCourse(Integer courseId, Integer teacherId);
+
+    void activateCourse(Integer id);
+
+    void deactivateCourse(Integer id);
 }
