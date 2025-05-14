b5f0491e4f758c5d8623cc0c45ece4311927630f
Wickram2001 <wickramdarshana2001@gmail.com>
2025-05-15 00:00:25 +0530

    Update StudentService.java

--- Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/service/StudentService.java
+++ Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/service/StudentService.java
@@ -1,4 +1,31 @@
 package com.ManagementApplication.StudentManagementSystem.service;
 
+import com.ManagementApplication.StudentManagementSystem.dto.StudentDTO;
+
+import java.util.List;
+
 public interface StudentService {
+
+    StudentDTO createStudent(StudentDTO studentDTO);
+
+    StudentDTO getStudentById(Integer id);
+
+    StudentDTO getStudentByStudentId(String studentId);
+
+    StudentDTO getStudentByUserId(Integer userId);
+
+    List<StudentDTO> getAllStudents();
+
+    List<StudentDTO> getStudentsByDepartment(String department);
+
+    List<StudentDTO> getStudentsByEnrollmentYear(Integer enrollmentYear);
+
+    List<StudentDTO> getStudentsByCurrentSemester(String currentSemester);
+
+    StudentDTO updateStudent(Integer id, StudentDTO studentDTO);
+
+    void deleteStudent(Integer id);
+
+    boolean isStudentIdExists(String studentId);
 }
+
