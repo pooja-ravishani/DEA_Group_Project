09242682ecf2a92720336298b02da409dcd839d4
Navindu2002 <navindumunasinghe2002@gmail.com>
2025-05-15 12:09:30 +0530

    Update TeacherDTO.java

--- Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/dto/TeacherDTO.java
+++ Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/dto/TeacherDTO.java
@@ -1,4 +1,38 @@
 package com.ManagementApplication.StudentManagementSystem.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class TeacherDTO {
+
+    private Integer id;
+
+    @NotNull(message = "User ID is required")
+    private Integer userId;
+
+    private UserDTO user;
+
+    @NotBlank(message = "Employee ID is required")
+    private String employeeId;
+
+    private String department;
+
+    private String specialization;
+
+    private String contactNumber;
+
+    private String officeLocation;
+
+    private String officeHours;
+
+    public Integer getUserId() {
+        return 0;
+    }
 }
