e43eda6d3a24797206a88b1cda1b0df23bd5daba
NimeshICT1886 <nimeshgithub1886@gmail.com>
2025-05-18 21:41:11 +0530

    Update StudentController.java

--- Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/controller/StudentController.java
+++ Backend/StudentManagementSystem/src/main/java/com/ManagementApplication/StudentManagementSystem/controller/StudentController.java
@@ -2,7 +2,7 @@ package com.ManagementApplication.StudentManagementSystem.controller;
 
 import com.ManagementApplication.StudentManagementSystem.dto.StudentDto;
 import com.ManagementApplication.StudentManagementSystem.exception.ResourceNotFoundException;
-import com.ManagementApplication.StudentManagementSystem.services.StudentService;
+import com.ManagementApplication.StudentManagementSystem.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
